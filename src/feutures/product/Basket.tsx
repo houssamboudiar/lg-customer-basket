0949df98b5c56b6d8062cd5adaacdd4139d61ebc
houssamboudiar <houssam.boudiar@univ-constantine2.dz>
2022-09-21 13:30:49 +0100

    Added Styled Components

--- src/feutures/product/Basket.tsx
+++ src/feutures/product/Basket.tsx
@@ -1,9 +1,16 @@
 import React from 'react'; // we need this to make JSX compile
+import styled from 'styled-components';
 
+export const Container = styled.div`
+    display: flex;
+    justify-content:space-evenly;
+    align-items: center;
+`
+ 
 export const Basket = () => {
   return (
-    <div>
+    <Container>
       <h1>Basket Component</h1>
-    </div>
+    </Container>
   )
 }
